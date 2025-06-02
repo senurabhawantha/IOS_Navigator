abf17154e7a09be4f184ce77474ec4a40de24d19
Senura Bhawantha <79014951+senurabhawantha@users.noreply.github.com>
2025-06-02 22:33:05 +0530

    Initial Commit

---
+++ IOS_Navigator/IOS_NavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_NavigatorApp.swift
+//  IOS_Navigator
+//
+//  Created by ITEDP on 2025-06-02.
+//
+
+import SwiftUI
+
+@main
+struct IOS_NavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
